e18d4e5b9ca9d7122bb8ccf24d3252fa12a5f3af
Alberto González <albgonzalez@gmail.com>
2011-12-12 19:04:20 +0100

    Nothing changed

--- sparams.c
+++ sparams.c
@@ -20,7 +20,7 @@ double complex *sparams(){
 double complex *mmatrix( int Nfreqs, int m1, int n1,double complex matrix1[m1][n1][Nfreqs], int m2, int n2,double complex matrix2[m2][n2][Nfreqs]){
 	double complex retMatrix[m1][n2][Nfreqs]=(double complex(*)[m1][n2][Nfreqs])malloc(m1*n2*Nfreqs*sizeof(double complex));
 	for(int i=0;i<m1;i++){
-		double complex partsum[Nfreqs]=0;
+		register double complex partsum[Nfreqs]=0;
 		for(int j=0;j<n2;j++){
 			for(int p=0;p<n1;p++){
 				for(int k=0;k<Nfreqs;k++){
